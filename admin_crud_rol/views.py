f5663609019a4c065bbeeec6e6e0522356b43dab
Sebax <sebadelsor@gmail.com>
2025-05-14 22:29:44 -0400

    Crud terminado

--- admin_crud_rol/views.py
+++ admin_crud_rol/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -44,7 +44,31 @@ def producto_activo_desactivado(request, id):
     except Producto.DoesNotExist:
         return JsonResponse({'error': 'Producto no encontrado'}, status=404)
 
+@api_view(['PUT'])
+def api_editar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
 
+    # Modifica el producto directamente sin almacenar el historial de precios
+    serializer = ProductoSerializer(producto, data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def editar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    return render(request, 'admin/admin-editar-producto.html', {
+        'producto': producto
+    })
+
+@api_view(['GET'])
+def api_lista_productos(request):
+    productos = Producto.objects.filter(estado=True)  # Solo los activos
+    serializer = ProductoSerializer(productos, many=True)  # Serializa los productos
+    return Response(serializer.data)
 
 
 class UserAdminView(APIView):
@@ -54,3 +78,21 @@ class UserAdminView(APIView):
         user.is_staff = not user.is_staff  # Alternar entre staff y no staff
         user.save()
         return Response({"message": "Permisos de usuario actualizados"}, status=status.HTTP_200_OK)
+    
+
+@api_view(['PATCH'])
+@login_required(login_url='/login/')
+def cambiar_rol(request, id):
+    if not request.user.is_staff:
+        return JsonResponse({'error': 'No tienes permisos para modificar productos.'}, status=403)
+    
+
+    
+
+    try:
+        usuario = User.objects.get(id=id)
+        usuario.is_staff =  not usuario.is_staff
+        usuario.save()
+        return JsonResponse({'mensaje': 'Estado del usuario actualizado correctamente', 'Rol': usuario.is_staff}, status=200)
+    except Producto.DoesNotExist:
+        return JsonResponse({'error': 'Rol no encontrado'}, status=404)
